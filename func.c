b6390ebe0990951025c540d5e4d7ad44587bbcd9
victorbxavier <victorxavier530@gmail.com>
2021-09-12 17:29:54 -0300

    Criação da função verifica parametros na func.c

--- func.c
+++ func.c
@@ -1,6 +1,6 @@
 #include "func.h"
 
-int valor_medio(int *array, int tamA, int pos1, int pos2)  {
+int valor_medio(int *array, int pos1, int pos2)  {
 
 	int diferenca=0, posMedia=0;
 	
@@ -16,10 +16,48 @@ int valor_medio(int *array, int tamA, int pos1, int pos2)  {
 
 		array[posMedia] = (array[pos1] + array[pos2])/2 + diferenca;
 
-		valor_medio(array, tamA, pos1, posMedia);
+		valor_medio(array, pos1, posMedia);
 		
-		valor_medio(array, tamA, posMedia, pos2);
+		valor_medio(array, posMedia, pos2);
 
 	}
 
+}
+
+void verificaParametros(int argc, char *argv[], int *tam, char *nomeArquivo){
+	
+	int existeD=0, existeO=0;
+
+	for (size_t i = 1; i < argc; i++){
+
+		if (strcmp(argv[i], "-d")==0){
+			
+			*tam=atoi(argv[i+1]);
+
+			existeD++;
+
+		}
+
+		if (strcmp(argv[i], "-o")==0){
+			nomeArquivo=malloc(strlen(argv[i+1])*sizeof(char));
+
+			strcpy(nomeArquivo, argv[i+1]);
+			nomeArquivo[strlen(argv[i+1])+1]='\0';
+			existeO++;
+			
+		}
+	}
+
+	if (existeD==0){
+		*tam=17;
+	}
+
+	if (existeO==0){
+
+		nomeArquivo=malloc(15*sizeof(char));
+		strcpy(nomeArquivo, "montanhas.ppm");
+		nomeArquivo[15]='\0';
+
+	}
+	
 }
