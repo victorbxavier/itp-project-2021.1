3386e464db8c517700315b35cfad9b2afe340c8a
victorbxavier <victorxavier530@gmail.com>
2021-09-12 17:33:42 -0300

    Permitindo que a função main receba comandos via linha de comando, mudança na função valor medio, criação da variavel que recebe o nome do arquivo e utilização da função verifica parametros.

--- projeto.c
+++ projeto.c
@@ -1,9 +1,18 @@
 #include "func.h"
 
-int main() {
+int main(int argc, char *argv[]) {
   
-  int tam = 17;
+  int tam = 0;
+
+  char *nomeArquivo;
+  
   int *contorno;
+
+  verificaParametros(argc, argv, &tam, nomeArquivo);
+
+  // for(size_t cont=1; cont < argc; cont++)
+  //   printf("%d Parametro: %s\n", cont,argv[cont]);
+
   contorno= malloc(tam * sizeof(int));
 
   srand(time(NULL));
@@ -11,16 +20,29 @@ int main() {
   contorno[0] = rand() % 1000;
   contorno[tam-1] = rand() % 1000;
 
-  valor_medio(contorno, tam, 0, tam-1);
+  valor_medio(contorno, 0, tam-1);
   
-  for (int i = 0; i < tam; i++)
-  {
-    /* code */
-    printf(" %d ", contorno[i]);
-  }
+  // for (int i = 0; i < tam; i++)
+  // {
+  //   /* code */
+  //   printf(" %d ", contorno[i]);
+  // }
   
+  // codigo teste
+  //fprinf(arquivo, "P3\n 1000 \n1000 \n 255 \n");
+  //for(int i=0; i<tamanhoImgHor; i++){
+  //   for(int j=0; j< tamanhoImgVer; j++){
+  //     if(linha>contorno[j]){
+  //        fprintf(arquivo, "119 130 186\n");
+  //      }
+          // else{
+          //   fprintf(arquivo, "159 178 255\n");
+          // }
+  //    }
+  //}
 
   free(contorno);
+  free(nomeArquivo);
 
   return 0;
 }
