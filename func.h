9261417a6986c80b4766738727daba3476266d8b
victorbxavier <victorxavier530@gmail.com>
2021-09-12 17:30:46 -0300

    Criação da função verificaParametros na func.h

--- func.h
+++ func.h
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
-int valor_medio (int *array, int tam, int pos1, int pos2);
+int valor_medio (int *array, int pos1, int pos2);
+void verificaParametros(int argc, char *argv[], int *tam, char *nomeArquivo);
 //Criar uma função para as diferenças ficarem sutis
 int diferencaArrays();
 
